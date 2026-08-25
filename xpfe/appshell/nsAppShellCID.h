6bf393b99e9bac7d271d5c0cb3da28b3d1180814
Daniel <drosas1904@gmail.com>
2026-08-25 02:02:56 -0700

    Update nsAppShellCID.h
    Signed-off-by: Daniel  <drosas1904@gmail.com>

--- xpfe/appshell/nsAppShellCID.h
+++ xpfe/appshell/nsAppShellCID.h
@@ -1,10 +1,3 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#define nsAppShellCID_h_#define NS_APPSHELLSERVICE_CONTRACTID "@mozilla.org/appshell
 
-#ifndef nsAppShellCID_h_
-#define nsAppShellCID_h_
 
-#define NS_APPSHELLSERVICE_CONTRACTID "@mozilla.org/appshell/appShellService;1"
-
-#endif
